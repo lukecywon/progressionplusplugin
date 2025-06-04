bebeb7c962946ee731d7117e07600a97aabe9f3f
eyap0015 <eyap0015@student.monash.edu>
2025-06-04 17:08:16 +0800

    Patched nocturnhood nightvision

--- src/main/kotlin/com/lukecywon/progressionPlus/listeners/NocturnHoodListener.kt
+++ src/main/kotlin/com/lukecywon/progressionPlus/listeners/NocturnHoodListener.kt
@@ -1,35 +1,49 @@
 package com.lukecywon.progressionPlus.listeners
 
+import com.lukecywon.progressionPlus.ProgressionPlus
 import com.lukecywon.progressionPlus.items.NocturnHood
-import org.bukkit.Material
-import org.bukkit.Particle
-import org.bukkit.Sound
+import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
-import org.bukkit.event.player.PlayerMoveEvent
 import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.event.player.PlayerQuitEvent
 import org.bukkit.potion.PotionEffect
 import org.bukkit.potion.PotionEffectType
+import org.bukkit.scheduler.BukkitRunnable
+import java.util.*
 
 class NocturnHoodListener : Listener {
 
-    private fun applyNightVision(player: Player) {
-        val helmet = player.inventory.helmet ?: return
-        if (NocturnHood.isNocturnHood(helmet)) {
-            player.addPotionEffect(
-                PotionEffect(PotionEffectType.NIGHT_VISION, 600, 0, true, false, false)
-            )
-        }
-    }
+    private val active = mutableSetOf<UUID>()
 
-    @EventHandler
-    fun onMove(e: PlayerMoveEvent) {
-        applyNightVision(e.player)
+    init {
+        object : BukkitRunnable() {
+            override fun run() {
+                for (player in Bukkit.getOnlinePlayers()) {
+                    val helmet = player.inventory.helmet ?: continue
+                    if (!NocturnHood.isNocturnHood(helmet)) {
+                        active.remove(player.uniqueId)
+                        continue
+                    }
+
+                    active.add(player.uniqueId)
+
+                    player.addPotionEffect(
+                        PotionEffect(PotionEffectType.NIGHT_VISION, 40, 0, false, false, false)
+                    )
+                }
+            }
+        }.runTaskTimer(ProgressionPlus.getPlugin(), 0L, 20L) // Runs every second
     }
 
     @EventHandler
     fun onJoin(e: PlayerJoinEvent) {
-        applyNightVision(e.player)
+        active.add(e.player.uniqueId)
+    }
+
+    @EventHandler
+    fun onQuit(e: PlayerQuitEvent) {
+        active.remove(e.player.uniqueId)
     }
 }
