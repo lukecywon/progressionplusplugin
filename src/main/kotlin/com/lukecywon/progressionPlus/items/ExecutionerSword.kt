f5ece9dff6ff671ab5b83925bd893e6d5f30ada3
stockcarrot <aurummc.yt@gmail.com>
2025-06-14 15:49:48 +0800

    Changed meta lore of ExecutionerSword

--- src/main/kotlin/com/lukecywon/progressionPlus/items/ExecutionerSword.kt
+++ src/main/kotlin/com/lukecywon/progressionPlus/items/ExecutionerSword.kt
@@ -1,6 +1,8 @@
 package com.lukecywon.progressionPlus.items
 
+import com.lukecywon.progressionPlus.enums.Activation
 import com.lukecywon.progressionPlus.enums.Rarity
+import com.lukecywon.progressionPlus.mechanics.ItemLore
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
 import org.bukkit.Material
@@ -16,8 +18,14 @@ object ExecutionerSword : CustomItem("executioner_sword", Rarity.EPIC) {
         meta.persistentDataContainer.set(key, PersistentDataType.BYTE, 1)
 
         meta.lore(listOf(
-            Component.text("The final sight of many men.", NamedTextColor.GRAY),
-            Component.text("Right-click to cleave in front of yourself!", NamedTextColor.YELLOW),
+//            Component.text("The final sight of many men.", NamedTextColor.GRAY),
+//            Component.text("Right-click to cleave in front of yourself!", NamedTextColor.YELLOW),
+            ItemLore.abilityuse("Final Verdict", Activation.RIGHT_CLICK),
+            ItemLore.description("Cleaves all enemies in front of you"),
+            ItemLore.cooldown(0),
+            ItemLore.stats(item),
+            ItemLore.separator(),
+            ItemLore.lore("The final sight of many men."),
         ))
 
         item.itemMeta = meta
