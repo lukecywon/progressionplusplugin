e93f6e465a4ec826b7f8634156b9df58c52e979d
eyap0015 <eyap0015@student.monash.edu>
2025-06-17 00:28:46 +0800

    Added new item refined eye

---
+++ src/main/kotlin/com/lukecywon/progressionPlus/items/progression/RefinedEye.kt
@@ -0,0 +1,47 @@
+package com.lukecywon.progressionPlus.items.component
+
+import com.lukecywon.progressionPlus.enums.Rarity
+import com.lukecywon.progressionPlus.items.CustomItem
+import com.lukecywon.progressionPlus.mechanics.ItemLore
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.format.TextDecoration
+import org.bukkit.Material
+import org.bukkit.inventory.ItemStack
+import org.bukkit.persistence.PersistentDataType
+
+object RefinedEye : CustomItem("custom_ender_eye", Rarity.COMPONENT) {
+
+    override fun createItemStack(): ItemStack {
+        val item = ItemStack(Material.ENDER_EYE)
+        val meta = item.itemMeta
+
+        meta.displayName(
+            Component.text("Refined Eye")
+                .color(NamedTextColor.DARK_PURPLE)
+                .decorate(TextDecoration.BOLD)
+        )
+
+        meta.lore(
+            listOf(
+                ItemLore.description("A crystallized version of the Ender Eye,"),
+                ItemLore.description("forged through immense magical compression."),
+                ItemLore.separator(),
+                ItemLore.lore("“It never breaks.”")
+            )
+        )
+
+        meta.persistentDataContainer.set(key, PersistentDataType.BYTE, 1)
+        item.itemMeta = meta
+
+        return applyMeta(item)
+    }
+
+    override fun getRecipe(): List<Material?> {
+        return listOf(
+            Material.DIAMOND, Material.DIAMOND, Material.DIAMOND,
+            Material.DIAMOND, Material.BLAZE_POWDER, Material.DIAMOND,
+            Material.DIAMOND, Material.DIAMOND, Material.DIAMOND
+        )
+    }
+}
