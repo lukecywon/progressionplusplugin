f233a1cc44552b50605535bb4f9616814bb4437f
Luke Won <lukecywon@gmail.com>
2025-06-20 02:00:31 +0800

    Made all components stackable

--- src/main/kotlin/com/lukecywon/progressionPlus/items/component/TideCrystal.kt
+++ src/main/kotlin/com/lukecywon/progressionPlus/items/component/TideCrystal.kt
@@ -10,7 +10,7 @@ import org.bukkit.Material
 import org.bukkit.NamespacedKey
 import org.bukkit.inventory.ItemStack
 
-object TideCrystal : CustomItem("tide_crystal", Rarity.COMPONENT) {
+object TideCrystal : CustomItem("tide_crystal", Rarity.COMPONENT, true) {
     override fun createItemStack(): ItemStack {
         val item = ItemStack(Material.PRISMARINE_CRYSTALS)
         val meta = item.itemMeta
