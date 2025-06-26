2dedbdf68c29f7d2e59e45f1e3ba6bdc511c3496
Luke Won <lukecywon@gmail.com>
2025-06-27 02:39:18 +0800

    Added Rocket Harness recipe

--- src/main/kotlin/com/lukecywon/progressionPlus/items/armor/epic/RocketHarness.kt
+++ src/main/kotlin/com/lukecywon/progressionPlus/items/armor/epic/RocketHarness.kt
@@ -3,6 +3,7 @@ package com.lukecywon.progressionPlus.items.armor.epic
 import com.lukecywon.progressionPlus.enums.Activation
 import com.lukecywon.progressionPlus.enums.Rarity
 import com.lukecywon.progressionPlus.items.CustomItem
+import com.lukecywon.progressionPlus.items.component.InfernalShard
 import com.lukecywon.progressionPlus.mechanics.ItemLore
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
@@ -13,6 +14,7 @@ import org.bukkit.attribute.Attribute
 import org.bukkit.attribute.AttributeModifier
 import org.bukkit.inventory.EquipmentSlotGroup
 import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.RecipeChoice
 
 object RocketHarness : CustomItem("rocket_harness", Rarity.EPIC, false) {
     override fun createItemStack(): ItemStack {
@@ -30,11 +32,19 @@ object RocketHarness : CustomItem("rocket_harness", Rarity.EPIC, false) {
                 ItemLore.abilityuse("Sorrowful March", Activation.PASSIVE),
                 ItemLore.description("Doubles the fly speed of a Happy Ghast"),
                 ItemLore.separator(),
-                ItemLore.lore("A remnant of ancient wrath, still echoing with vengeance."),
+                ItemLore.lore("A harness with a rocket strapped onto it's back."),
             )
         )
 
         item.itemMeta = meta
         return applyMeta(item)
+    }
+
+    override fun getRecipe(): List<RecipeChoice?> {
+        return listOf(
+            RecipeChoice.MaterialChoice(Material.REDSTONE), RecipeChoice.MaterialChoice(Material.WHITE_HARNESS), RecipeChoice.MaterialChoice(Material.REDSTONE),
+            RecipeChoice.MaterialChoice(Material.REDSTONE), RecipeChoice.ExactChoice(InfernalShard.createItemStack()), RecipeChoice.MaterialChoice(Material.REDSTONE),
+            RecipeChoice.MaterialChoice(Material.REDSTONE), RecipeChoice.MaterialChoice(Material.REDSTONE), RecipeChoice.MaterialChoice(Material.REDSTONE)
+        )
     }
 }
