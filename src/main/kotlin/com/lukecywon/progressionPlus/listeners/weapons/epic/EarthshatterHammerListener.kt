af4111350388d73195432f48edddcf1e00fa02d2
eyap0015 <eyap0015@student.monash.edu>
2025-06-27 02:50:17 +0800

    Added 30s cooldown to EarthshatterHammer

--- src/main/kotlin/com/lukecywon/progressionPlus/listeners/weapons/epic/EarthshatterHammerListener.kt
+++ src/main/kotlin/com/lukecywon/progressionPlus/listeners/weapons/epic/EarthshatterHammerListener.kt
@@ -1,6 +1,7 @@
 package com.lukecywon.progressionPlus.listeners.weapons.epic
 
 import com.lukecywon.progressionPlus.ProgressionPlus
+import com.lukecywon.progressionPlus.items.CustomItem
 import com.lukecywon.progressionPlus.items.weapons.epic.EarthshatterHammer
 import org.bukkit.*
 import org.bukkit.entity.FallingBlock
@@ -19,6 +20,8 @@ import kotlin.math.*
 
 class EarthshatterHammerListener : Listener {
     private val plugin = ProgressionPlus.getPlugin()
+    private val itemId = "earthshatter_hammer"
+    private val cooldownMillis = 30_000L
 
     @EventHandler
     fun onRightClick(e: PlayerInteractEvent) {
@@ -27,6 +30,14 @@ class EarthshatterHammerListener : Listener {
 
         if (!EarthshatterHammer.isEarthshatterHammer(item)) return
         if (e.action != Action.RIGHT_CLICK_AIR && e.action != Action.RIGHT_CLICK_BLOCK) return
+
+        if (CustomItem.isOnCooldown(itemId, player.uniqueId)) {
+            val millisLeft = CustomItem.getCooldownRemaining(itemId, player.uniqueId)
+            val minutes = (millisLeft / 1000) / 60
+            val seconds = (millisLeft / 1000) % 60
+            player.sendMessage("§cEarthshatter is on cooldown for §e${minutes}m ${seconds}s§c!")
+            return
+        }
 
         e.isCancelled = true
 
@@ -38,6 +49,7 @@ class EarthshatterHammerListener : Listener {
             override fun run() {
                 // Execute earthshatter visual + enemy slowness
                 launchEarthshatter(player)
+                CustomItem.setCooldown(itemId, player.uniqueId, cooldownMillis) // apply cooldown after activation
             }
         }.runTaskLater(plugin, 20L) // 1.5 seconds later
     }
