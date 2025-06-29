b78634b60efc5a89b72e3b6596ff32d5d476201a
Luke Won <lukecywon@gmail.com>
2025-06-30 00:57:41 +0800

    Changed SetupManager to use classpath scanning to find all Managers

--- src/main/kotlin/com/lukecywon/progressionPlus/setup/SetupManagers.kt
+++ src/main/kotlin/com/lukecywon/progressionPlus/setup/SetupManagers.kt
@@ -3,19 +3,17 @@ package com.lukecywon.progressionPlus.setup
 import com.lukecywon.progressionPlus.annotations.RunOnEnable
 import com.lukecywon.progressionPlus.mechanics.*
 import org.bukkit.plugin.java.JavaPlugin
+import org.reflections.Reflections
 
 class SetupManagers {
     @RunOnEnable
     private fun managers(plugin: JavaPlugin) {
-        val mechanics = listOf<Manager>(
-            BerserkerSwordManager,
-            FlightBeaconManager,
-            LegendaryManager,
-            DiamondRecipeManager
-        )
+        val managers = Reflections("com.lukecywon.progressionPlus.manager")
+        val classes = managers.getSubTypesOf(Manager::class.java)
 
-        mechanics.forEach {
-            it.start(plugin)
+        classes.forEach {
+            val classInstance = it.getDeclaredConstructor().newInstance()
+            classInstance.start(plugin)
         }
     }
 }
