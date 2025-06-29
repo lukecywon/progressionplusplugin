1b181a5f4f0c511f4800666b44c1bd9d1a54935c
Luke Won <lukecywon@gmail.com>
2025-06-30 00:29:03 +0800

    Created RunOnEnable annotation to mark any method to be run on plugin enable

---
+++ src/main/kotlin/com/lukecywon/progressionPlus/annotations/RunOnEnable.kt
@@ -0,0 +1,8 @@
+package com.lukecywon.progressionPlus.annotations
+
+/**
+ * Annotation representing methods to be run on plugin start
+ */
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class RunOnEnable()
