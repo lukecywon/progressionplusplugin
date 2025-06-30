a9c0a407e66f374f3c1236dda507d1b7ae1f86ad
eyap0015 <eyap0015@student.monash.edu>
2025-07-01 02:35:44 +0800

    Removed vanilla ender chest and end crystal recipe

--- src/main/kotlin/com/lukecywon/progressionPlus/recipes/EnderChestRecipe.kt
+++ src/main/kotlin/com/lukecywon/progressionPlus/recipes/EnderChestRecipe.kt
@@ -9,27 +9,6 @@ import org.bukkit.inventory.ShapedRecipe
 
 object EnderChestRecipe : Recipe {
     override fun register() {
-        val key = Material.ENDER_CHEST.key
-        Bukkit.removeRecipe(key)
-
-        val newKey = NamespacedKey(ProgressionPlus.getPlugin(), "ender_chest")
-        val result = ItemStack(Material.ENDER_CHEST)
-
-        if (Bukkit.getRecipe(key) != null) {
-            Bukkit.removeRecipe(key)
-        }
-
-        val recipe = ShapedRecipe(key, result)
-
-        recipe.shape(
-            "OOO",
-            "OEO",
-            "OOO"
-        )
-
-        recipe.setIngredient('O', Material.OBSIDIAN)
-        recipe.setIngredient('E', Material.ENDER_PEARL)
-
-        Bukkit.addRecipe(recipe)
+        Bukkit.removeRecipe(NamespacedKey.minecraft("ender_chest"))
     }
 }
