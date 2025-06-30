2116b991889da422be1d88ca1473532c6a08428e
Luke Won <lukecywon@gmail.com>
2025-07-01 01:28:46 +0800

    Moved Ferocious Blade to Legendary status

--- src/main/kotlin/com/lukecywon/progressionPlus/listeners/weapons/legendary/FerociousBladeListener.kt
+++ src/main/kotlin/com/lukecywon/progressionPlus/listeners/weapons/legendary/FerociousBladeListener.kt
@@ -1,4 +1,4 @@
-package com.lukecywon.progressionPlus.listeners.weapons.epic
+package com.lukecywon.progressionPlus.listeners.weapons.legendary
 
 import com.lukecywon.progressionPlus.ProgressionPlus
 import com.lukecywon.progressionPlus.items.weapons.epic.FerociousBlade
