00749f0ce0fbc70a7141649026337922437b8798
Luke Won <lukecywon@gmail.com>
2025-07-02 22:35:30 +0800

    Made nether eye only unusable after being used once.

--- src/main/kotlin/com/lukecywon/progressionPlus/listeners/utility/progression/NetherEyeListener.kt
+++ src/main/kotlin/com/lukecywon/progressionPlus/listeners/utility/progression/NetherEyeListener.kt
@@ -1,12 +1,17 @@
-package com.lukecywon.progressionPlus.listeners.utility.legendary
+package com.lukecywon.progressionPlus.listeners.utility.progression
 
+import com.lukecywon.progressionPlus.ProgressionPlus
 import com.lukecywon.progressionPlus.items.progression.NetherEye
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.Action
 import org.bukkit.event.player.PlayerInteractEvent
 
 class NetherEyeListener : Listener {
+    private val plugin = ProgressionPlus.getPlugin()
+    private val config = plugin.config
 
     @EventHandler
     fun onRightClick(e: PlayerInteractEvent) {
@@ -16,8 +21,17 @@ class NetherEyeListener : Listener {
         val item = player.inventory.itemInMainHand
         if (!NetherEye.isThisItem(item)) return
 
+        // Nether eye only usable once
+        if (config.getBoolean("nether_opened")) {
+            player.sendMessage(Component.text("The Nether has already been unlocked.").color(NamedTextColor.DARK_RED))
+            e.isCancelled = true
+            return
+        }
+
         // Trigger logic
         NetherEye.toggleNether(player)
+        config.set("nether_opened", true)
+
 
         // Consume the item
         item.amount = item.amount - 1
